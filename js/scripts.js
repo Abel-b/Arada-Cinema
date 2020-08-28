73246543f21ffa4687e6a6fdef8aff6fdccdb7b7
Fasika <abelberhane011@gmail.com>
2020-08-28 23:53:10 +0300

    adds sign in page, functional watch later and book structures

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+function myFunction1() {
+    var popup = document.getElementById("myPopup1");
+    popup.classList.toggle("show");
+  }
+  function myFunction2() {
+    var popup = document.getElementById("myPopup2");
+    popup.classList.toggle("show");
+  }
+  function myFunction3() {
+    var popup = document.getElementById("myPopup3");
+    popup.classList.toggle("show");
+  }
+  function myFunction4() {
+    var popup = document.getElementById("myPopup4");
+    popup.classList.toggle("show");
+  }
+  function myFunction5() {
+    var popup = document.getElementById("myPopup5");
+    popup.classList.toggle("show");
+  }
+  function myFunction6() {
+    var popup = document.getElementById("myPopup6");
+    popup.classList.toggle("show");
+  }
+  function myFunction7() {
+  var popup = document.getElementById("myPopup7");
+  popup.classList.toggle("show");
+}
+function myFunction8() {
+    var popup = document.getElementById("myPopup8");
+    popup.classList.toggle("show");
+}
+
+            
